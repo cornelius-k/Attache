f61acfca6cf135fe27779d790d8dc8d7d322a42d
Neil Kempin <neil.kempin@mdaus.com>
2016-07-11 23:31:48 -0400

    sheer onslaught of development: content-tools plugin, three_column template and more

--- app/assets/javascripts/sheets.js
+++ app/assets/javascripts/sheets.js
@@ -29,9 +29,38 @@ function initDragUpload(holder){
 
 }
 
+
+function getTweetData(){
+    var obj = [];
+    obj.push({
+      "twitter_handle": "TwitterUserOne",
+      "tweet_text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin, magna sit amet dictum convallis, urna leo efficitur ligula, a convallis mi tortor vel dui.",
+      "time": "Monday Jul 11"});
+    obj.push({
+      "twitter_handle": 'TwitterUserTwo',
+      "tweet_text": 'In hac habitasse platea dictumst. In pulvinar vehicula quam, non faucibus orci mattis vitae. Suspendisse consectetur massa a tincidunt egestas.',
+      "time": 'Tuesday Jul 12'
+    });
+    obj.push({
+      "twitter_handle": 'TwitterUserThree',
+      "tweet_text": "Maecenas efficitur massa quis nisi tristique volutpat. Vestibulum eget volutpat mi, in faucibus nisl. Vestibulum ante nulla, tempus eget massa at, vulputate eleifend erat. Integer pharetra non lorem a pulvinar. Nunc gravida erat ante, ut tincidunt nisl ultricies ut. ",
+      "time": "Monday Jul 11"
+    });
+    
+    return obj;
+
+}
+
+function showTweetData(e){
+  e.preventDefault();
+  $('.code-zone').text(JSON.stringify(getTweetData()));
+}
+
+
 $(document).ready(function(){
   $('.drop-upload').each(function(){
     initDragUpload(this);
-
+    editor = ContentTools.EditorApp.get();
+    editor.init('[data-editable], [data-fixture]', 'data-name');
   });
 });
