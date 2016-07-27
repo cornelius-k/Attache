db730b1386bf79e345eed0661f7e3ec23b4005f4
Neil Kempin <neil.kempin@mdaus.com>
2016-07-27 17:38:19 -0400

    remove migration files -- database load from schema.rb is expected for initializing application from this point

--- db/migrate/20160710024241_remove_columns_from_sections.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColumnsFromSections < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :sections, :string, :text
-    remove_column :sections, :sheet, :text
-    remove_column :sections, :references, :text
-  end
-end
--- db/migrate/20160710024417_remove_columns_from_sheets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnsFromSheets < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :sheets, :text, :text
-  end
-end
